5c97cad645d57672a1fb23fa2148181836972b0b
ValeriaCA9 <vale38646@gmail.com>
2025-03-20 14:50:06 -0600

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -1,4 +1,50 @@
 //Cruz Alegria Valeria Montserrat
 //Calculadora-Ejercicio Switch
 
-studio.
+#include <stdio.h>
+
+int main()
+  a=0
+  b=0
+  op=0
+  
+{
+ Printf("Escoge una opcion: ")
+   scanf("%op" &enteronumero)
+   
+  if (op=1);
+{
+printf("Lectura de primer numero: );
+  scanf("a");
+printf("Lectura de segundo numero: );
+  scanf("b");
+printf(res="a"+"b");
+}
+else (op=2);
+{
+printf("Lectura de primer numero: );
+  scanf("a");
+printf("Lectura de segundo numero: );
+  scanf("b");
+printf(res="a"-"b");
+}
+if (op=3)
+  {
+printf("Lectura de primer numero: );
+  scanf("a");
+printf("Lectura de segundo numero: );
+  scanf("b");
+printf(res="a"*"b");
+}
+  else(op=4)
+     {
+printf("Lectura de primer numero: );
+  scanf("a");
+printf("Lectura de segundo numero: );
+  scanf("b");
+printf(res="a"/"b");
+}
+while(op=0)
+  
+
+    
